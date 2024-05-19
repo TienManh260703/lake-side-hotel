36a078fd8dde13246fa85ca7c0057fc3d4d8228f
manh_03 <nguyenmanh19924@gmail.com>
2024-05-19 17:43:22 +0700

    create back-end

---
+++ back-end/src/main/java/com/example/lakesidehotel/repository/BookedRoomRepository.java
@@ -0,0 +1,7 @@
+package com.example.lakesidehotel.repository;
+
+import com.example.lakesidehotel.model.BookedRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookedRoomRepository  extends JpaRepository<BookedRoom, Integer> {
+}
---
+++ back-end/src/main/java/com/example/lakesidehotel/service/impl/RoomServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.lakesidehotel.service.impl;
+
+import com.example.lakesidehotel.service.IRoomService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomServiceImpl implements IRoomService {
+}
